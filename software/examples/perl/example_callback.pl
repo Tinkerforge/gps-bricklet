36052ed2e46ab1ac566bdcc873fb45954ac70488
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 15:47:43 +0100

    Add Perl examples

---
+++ software/examples/perl/example_callback.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletGPS;
+
+use constant HOST => '192.168.178.29';
+use constant PORT => 4223;
+use constant UID => 'f9b'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $gps = BrickletGPS->new(&UID, $ipcon); # Create device object
+
+# Callback function for coordinates
+sub cb_coordinates
+{
+    my ($latitude, $ns, $longitude, $ew, $pdop, $hdop, $vdop, $epe) = @_;
+
+    print "\nLatitude: ".$latitude/1000000.0.'° '.$ns;
+    print "\nLongitude: ".$longitude/1000000.0.'° '.$ew."\n";
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Set Period for coordinates callback to 1s (1000ms)
+# Note: The callback is only called every second if the 
+#       coordinates have changed since the last call!
+$gps->set_coordinates_callback_period(1000);
+
+# Register coordinates callback to function cb_coordinates
+$gps->register_callback($gps->CALLBACK_COORDINATES, 'cb_coordinates');
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
---
+++ software/examples/perl/example_simple.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletGPS;
+
+use constant HOST => '192.168.178.29';
+use constant PORT => 4223;
+use constant UID => 'f9b'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $gps = BrickletGPS->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Get current coordinates
+my ($latitude, $ns, $longitude, $ew, $pdop, $hdop, $vdop, $epe) = $gps->get_coordinates();
+
+print "\nLatitude: ".$latitude/1000000.0.'° '.$ns;
+print "\nLongitude: ".$longitude/1000000.0.'° '.$ew."\n";
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
