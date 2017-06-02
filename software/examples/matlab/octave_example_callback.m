0fb865777efd346547903c32c0cf3967eedb17d5
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:08:38 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_callback.m
+++ software/examples/matlab/octave_example_callback.m
@@ -5,8 +5,8 @@ function octave_example_callback()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your GPS Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    gps = java_new("com.tinkerforge.BrickletGPS", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    gps = javaObject("com.tinkerforge.BrickletGPS", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
