1f362d8350451150490fa741544f652a71e5011b
Erik Fleckstein <erik@tinkerforge.com>
2020-01-13 17:17:01 +0100

    examples: Use python3 input instead of python2 raw_input.

--- software/examples/python/example_callback.py
+++ software/examples/python/example_callback.py
@@ -31,5 +31,5 @@ if __name__ == "__main__":
     #       if the coordinates has changed since the last call!
     gps.set_coordinates_callback_period(1000)
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
--- software/examples/python/example_simple.py
+++ software/examples/python/example_simple.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
     print("Longitude: " + str(longitude/1000000.0) + " °")
     print("E/W: " + ew)
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
